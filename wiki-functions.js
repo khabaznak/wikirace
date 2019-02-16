2f5e6c3bffe948023bf883b234748a5d009f159c
Alex Gomez <alegomez@homeaway.com>
2019-02-16 12:52:17 -0600

    Safety commit...

--- wiki-functions.js
+++ wiki-functions.js
@@ -2,34 +2,61 @@
 const axios = require('axios');
 const HTMLParser = require('node-html-parser');
 
-exports.getWikiArticleLinks = (articleUrl, responseObject)=>{ //TODO: response object will not be needed here in this function.
-    axios.get(articleUrl) 
-    .then(response => {
-            var article = HTMLParser.parse(response.data);
-            var links = article.querySelectorAll('a') //TODO: Need to make sure you're only getting the links from body and avoiding the sidebar and footer.
-                                .map((l)=>{return l.rawAttrs;})
-                                .filter((l) => {return l.includes('https');}) //TODO: should also filter out any link which goes outside wikipedia
-                                .map((l) => { 
-                                    return l.split(' ')
-                                            .filter(s=>{return s.includes('https');})[0];
-                                    })
-                                .map((l) => {
-                                    return l.replace('href="','')
-                                            .replace('"','');
-                                });
-            console.log(links);
-            
-            responseObject.send(links);
-        })
-        .catch(error => {
-            console.log('catch',error);
-        });
-    
+const FIRST_HEADER_SELECTOR = '#firstHeading';
+const WIKI_ARTICLE_MAIN_BODY_SELECTOR = 'div#bodyContent';
+const WIKI_LINKS_SELECTOR = 'a';
+const HTTPS_STRING = 'https';
+const WIKIPEDIA_LINK_STRING = '/wiki/';
+const HREF_STRING = 'href="';
+const CLOSING_QUOTE_STRING = '"';
+
+/**Returns an Array with all the valid links from the wikiArticle object 
+ * which is passed as a parameter */
+exports.getWikiArticleLinks = (wikiArticle) => {
+    let articleBody = wikiArticle.querySelector(WIKI_ARTICLE_MAIN_BODY_SELECTOR);
+    var links = articleBody.querySelectorAll(WIKI_LINKS_SELECTOR)
+                            .map((l) => {return l.rawAttrs;}) //Getting raw attributes
+                            .filter((l) => {return l.includes(HTTPS_STRING);})
+                            .map((l) => { 
+                                return l.split(' ')
+                                        .filter(s => {
+                                            return s.includes(HTTPS_STRING) && s.includes(WIKIPEDIA_LINK_STRING);
+                                        })[0];
+                            })
+                            .map((l) => {
+                                return l.replace(HREF_STRING,'')
+                                .replace(CLOSING_QUOTE_STRING,'');
+                            });
+    return links;
 };
 
-exports.getLinkTree = () =>{
-    return {rootName='', children:[]};
+function LinkTreeNode(title, id, childrenNodes) {
+    this.title = title;
+    this.id = id;
+    this.children = childrenNodes;
 };
-// TODO: Create a function for getting articles
-// TODO: Create a function for getting Article title
+
+/*This function returns a parsed HTML Parser element object which represents 
+the Wiki Article in question via a callback function which is allso passed as parameter.
+@param {function} callback is the callback function where article will be returned.*/
+exports.getParsedWikiArticle = (articleUrl, callback) => {
+    return axios.get(articleUrl)
+    .then((response) => {
+        var article = HTMLParser.parse(response.data);
+        callback(article);
+    })
+    .catch((error) => { //TODO: Please note that this catch clause might catch problems in the axios get and also parsing problems...
+        console.log(`Ooops! some weird problem occured: here your stacktrace:\n${error}`);
+    });
+};
+
+/**This function returns the Title of the wiki Article in plain text. 
+ * the wikiArticle is an HTML Parser Element object */
+exports.getWikiArticleTitle = (wikiArticle) => {
+    return wikiArticle.querySelector(FIRST_HEADER_SELECTOR).text;
+};
+
 // TODO: Create a function which determines if winning condition has been reached
+exports.isRaceOver = () => {
+ return false;
+};
--- wikirace.js
+++ wikirace.js
@@ -9,9 +9,16 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //TODO: review the req object to obtain the articles in the request body
     //var x = wikiRacer.getWikiArticleLinks('https://en.wikipedia.org/wiki/Tesla',res);
-    var y = wikiRacer.getWikiArticleLinks('https://en.wikipedia.org/wiki/Elon_Musk',res);
+    //var y = wikiRacer.getWikiArticleLinks('https://en.wikipedia.org/wiki/Elon_Musk',res);
+    let articleA;
+    wikiRacer.getParsedWikiArticle('https://en.wikipedia.org/wiki/Tesla', 
+                                    (art)=>{
+                                        articleA = art;
+                                        let title = wikiRacer.getWikiArticleTitle(articleA);
+                                        res.send(`<H1>The article in question has a title: ${title}</H1>`);
+                                    });
 });
 
 app.listen(PORT, HOST);
